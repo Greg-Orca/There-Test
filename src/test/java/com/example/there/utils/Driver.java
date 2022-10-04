d462cff385f576eb3da662b00946608f57b6209a
gergelybartha <gergely.bartha95@gmail.com>
2022-10-04 15:56:44 +0200

    initial

---
+++ src/test/java/com/example/there/utils/Driver.java
@@ -0,0 +1,22 @@
+package com.example.there.utils;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Driver {
+    private static WebDriver driver = null;
+
+    private Driver(){}
+
+    public static WebDriver getDriver(){
+        if (driver ==null){
+            driver = new ChromeDriver();
+        }
+        return driver;
+    }
+
+    public static void quitDriver(){
+        driver.quit();
+        driver = null;
+    };
+}
